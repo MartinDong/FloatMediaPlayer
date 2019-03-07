744cf56f94ac8293fc41b1f2bb782b157f5bdc03
MartinDong <1358506549@qq.com>
2019-03-08 00:11:34 +0800

    Change song list

--- app/src/main/java/com/dong/floatmediaplayer/SongHiBaiListActivity.kt
+++ app/src/main/java/com/dong/floatmediaplayer/SongHiBaiListActivity.kt
@@ -1,17 +1,34 @@
 package com.dong.floatmediaplayer
 
+import android.annotation.SuppressLint
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
+import com.dong.floatmediaplayer.adapter.SongHiBaiListAdapter
 import com.dong.floatmediaplayer.base.BaseMvpActivity
 import com.dong.floatmediaplayer.bean.wangyi.Song
+import com.dong.floatmediaplayer.bean.wangyi.SongListResponse
 import com.dong.floatmediaplayer.contract.SongHiBaiListContract
 import com.dong.floatmediaplayer.presenter.SongHiBaiListPresenter
+import kotlinx.android.synthetic.main.activity_song_hi_bai_list.*
 
 class SongHiBaiListActivity : BaseMvpActivity<SongHiBaiListPresenter>(), SongHiBaiListContract.View {
+
+    lateinit var mSongListAdapter: SongHiBaiListAdapter
+    private var mSongList: MutableList<Song> = mutableListOf()
 
     override fun getLayoutId(): Int {
         return R.layout.activity_song_hi_bai_list
     }
 
+    @SuppressLint("WrongConstant")
     override fun initView() {
+        mSongListAdapter = SongHiBaiListAdapter(mSongList)
+
+        val layoutManager = LinearLayoutManager(this, VERTICAL, false)
+
+        rv_song_list.adapter = mSongListAdapter
+        rv_song_list.layoutManager = layoutManager
+
         mPresenter = SongHiBaiListPresenter()
         mPresenter!!.attachView(this)
         mPresenter!!.getSongList()
@@ -35,5 +52,7 @@ class SongHiBaiListActivity : BaseMvpActivity<SongHiBaiListPresenter>(), SongHiB
 
     override fun onSuccess(message: Any) {
         println("=======onSuccess========$message")
+        mSongList.addAll((message as SongListResponse).data.songs)
+        mSongListAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/dong/floatmediaplayer/adapter/SongHiBaiListAdapter.kt
+++ app/src/main/java/com/dong/floatmediaplayer/adapter/SongHiBaiListAdapter.kt
@@ -1,23 +1,37 @@
 package com.dong.floatmediaplayer.adapter
 
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.dong.floatmediaplayer.R
+import com.dong.floatmediaplayer.bean.wangyi.Song
 
-class SongHiBaiListAdapter : RecyclerView.Adapter<SongHiBaiListAdapter.SongHiBaiHV>() {
+class SongHiBaiListAdapter(private var mSongList: List<Song>) : RecyclerView.Adapter<SongHiBaiListAdapter.SongHiBaiHV>() {
+
+    @SuppressLint("InflateParams")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongHiBaiHV {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return SongHiBaiHV(LayoutInflater.from(parent.context).inflate(R.layout.item_song_hi_bai_list, parent, false))
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return if (mSongList.isEmpty()) {
+            0
+        } else {
+            mSongList.size
+        }
     }
 
     override fun onBindViewHolder(holder: SongHiBaiHV, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.initViewDate(mSongList[position])
     }
 
     class SongHiBaiHV(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        fun initViewDate(song: Song) {
+            itemView.findViewById<TextView>(R.id.tv_song_name).text = song.name
+            itemView.findViewById<TextView>(R.id.tv_song_singer).text = song.singer
+        }
     }
 }
